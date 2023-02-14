0b9e9aeba33bc30c69bb1116adee583ab352eff9
MaxKsyunz <maxk@bitquilltech.com>
2023-02-14 10:39:37 -0800

    Clean-up formatting, refactor some tests.
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- exporter/opensearchexporter/sso_trace_exporter_test.go
+++ exporter/opensearchexporter/sso_trace_exporter_test.go
@@ -24,6 +24,8 @@ import (
 	"github.com/antchfx/jsonquery"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 )
@@ -36,20 +38,20 @@ func Test_span_sample(t *testing.T) {
 		givenSpan:     sampleSpan(),
 		givenScope:    sampleScope(),
 		assertNode: func(t *testing.T, doc *jsonquery.Node) {
-			assertJsonValueEqual(t, "ip_tcp", doc, "/attributes/net.transport")
-			assertJsonValueEqual(t, float64(3), doc, "/droppedAttributesCount")
-			assertJsonValueEqual(t, float64(5), doc, "/droppedEventsCount")
-			assertJsonValueEqual(t, float64(8), doc, "/droppedLinksCount")
-			assertJsonValueEqual(t, sampleTimeB.Format(time.RFC3339Nano), doc, "/endTime")
-			assertJsonValueEqual(t, "Internal", doc, "/kind")
-			assertJsonValueEqual(t, "sample-span", doc, "/name")
-			assertJsonValueEqual(t, sampleSpanIdB.String(), doc, "/parentSpanId")
-			assertJsonValueEqual(t, sampleSpanIdA.String(), doc, "/spanId")
-			assertJsonValueEqual(t, "Error", doc, "/status/code")
-			assertJsonValueEqual(t, "sample status message", doc, "/status/message")
-			assertJsonValueEqual(t, sampleTimeA.Format(time.RFC3339Nano), doc, "/startTime")
-			assertJsonValueEqual(t, rawSampleTraceState, doc, "/traceState")
-			assertJsonValueEqual(t, sampleTraceIdA.String(), doc, "/traceId")
+			assertJSONValueEqual(t, "ip_tcp", doc, "/attributes/net.transport")
+			assertJSONValueEqual(t, float64(3), doc, "/droppedAttributesCount")
+			assertJSONValueEqual(t, float64(5), doc, "/droppedEventsCount")
+			assertJSONValueEqual(t, float64(8), doc, "/droppedLinksCount")
+			assertJSONValueEqual(t, sampleTimeB.Format(time.RFC3339Nano), doc, "/endTime")
+			assertJSONValueEqual(t, "Internal", doc, "/kind")
+			assertJSONValueEqual(t, "sample-span", doc, "/name")
+			assertJSONValueEqual(t, sampleSpanIDB.String(), doc, "/parentSpanId")
+			assertJSONValueEqual(t, sampleSpanIDA.String(), doc, "/spanId")
+			assertJSONValueEqual(t, "Error", doc, "/status/code")
+			assertJSONValueEqual(t, "sample status message", doc, "/status/message")
+			assertJSONValueEqual(t, sampleTimeA.Format(time.RFC3339Nano), doc, "/startTime")
+			assertJSONValueEqual(t, rawSampleTraceState, doc, "/traceState")
+			assertJSONValueEqual(t, sampleTraceIDA.String(), doc, "/traceId")
 		},
 	}
 
@@ -69,8 +71,8 @@ func Test_instrumentation_scope(t *testing.T) {
 				assert.NotNil(t, scope)
 				assert.NotNil(t, jsonquery.FindOne(scope, "name"))
 				assert.NotNil(t, jsonquery.FindOne(scope, "version"))
-				assertJsonValueEqual(t, sampleSchemaUrl, scope, "/schemaUrl")
-				assertJsonValueEqual(t, "com.example.some-sdk", scope, "/attributes/scope.short_name")
+				assertJSONValueEqual(t, sampleSchemaURL, scope, "/schemaUrl")
+				assertJSONValueEqual(t, "com.example.some-sdk", scope, "/attributes/scope.short_name")
 			},
 		},
 	}
@@ -103,11 +105,11 @@ func Test_links(t *testing.T) {
 			assertNode: func(t *testing.T, doc *jsonquery.Node) {
 				links := jsonquery.FindOne(doc, "/links/*")
 
-				assertJsonValueEqual(t, sampleTraceIdB.String(), links, "/traceId")
-				assertJsonValueEqual(t, sampleSpanIdB.String(), links, "/spanId")
-				assertJsonValueEqual(t, float64(3), links, "droppedAttributesCount")
-				assertJsonValueEqual(t, rawSampleTraceState, links, "traceState")
-				assertJsonValueEqual(t, "remote-link", links, "/attributes/link_attr")
+				assertJSONValueEqual(t, sampleTraceIDB.String(), links, "/traceId")
+				assertJSONValueEqual(t, sampleSpanIDB.String(), links, "/spanId")
+				assertJSONValueEqual(t, float64(3), links, "droppedAttributesCount")
+				assertJSONValueEqual(t, rawSampleTraceState, links, "traceState")
+				assertJSONValueEqual(t, "remote-link", links, "/attributes/link_attr")
 			},
 		},
 	}
@@ -139,9 +141,9 @@ func Test_events(t *testing.T) {
 			givenSpan:     withEventWithTimestamp(sampleSpan()),
 			assertNode: func(t *testing.T, doc *jsonquery.Node) {
 				eventsNode := jsonquery.FindOne(doc, "/events/*")
-				assertJsonValueEqual(t, "cloudevent", eventsNode, "name")
+				assertJSONValueEqual(t, "cloudevent", eventsNode, "name")
 				// JSON Go encodes all numbers as floats, see https://pkg.go.dev/encoding/json#Marshal
-				assertJsonValueEqual(t, float64(42), eventsNode, "droppedAttributesCount")
+				assertJSONValueEqual(t, float64(42), eventsNode, "droppedAttributesCount")
 				assert.NotNil(t, eventsNode.SelectElement("@timestamp"))
 				assert.Nil(t, eventsNode.SelectElement("observedTimestamp"))
 			},
@@ -154,7 +156,7 @@ func Test_events(t *testing.T) {
 			givenSpan:     withEventNoTimestamp(sampleSpan()),
 			assertNode: func(t *testing.T, doc *jsonquery.Node) {
 				eventsNode := jsonquery.FindOne(doc, "/events/*")
-				assertJsonValueEqual(t, "exception", eventsNode, "name")
+				assertJSONValueEqual(t, "exception", eventsNode, "name")
 				assert.NotNil(t, eventsNode.SelectElement("observedTimestamp"))
 				assert.Nil(t, eventsNode.SelectElement("@timestamp"))
 			},
@@ -191,7 +193,7 @@ func Test_attributes_data_stream(t *testing.T) {
 			name:      "datatset is ngnix",
 			configMod: func(config *Config) { config.Dataset = "ngnix" },
 			assertNode: func(t *testing.T, doc *jsonquery.Node) {
-				assertJsonValueEqual(t, "ngnix", doc, "/attributes/data_stream/dataset")
+				assertJSONValueEqual(t, "ngnix", doc, "/attributes/data_stream/dataset")
 				validateDataStreamType(t, doc)
 			},
 		},
@@ -200,7 +202,7 @@ func Test_attributes_data_stream(t *testing.T) {
 			configMod: func(config *Config) { config.Namespace = "exceptions" },
 			assertNode: func(t *testing.T, doc *jsonquery.Node) {
 				// no attributes property expected
-				assertJsonValueEqual(t, "exceptions", doc, "/attributes/data_stream/namespace")
+				assertJSONValueEqual(t, "exceptions", doc, "/attributes/data_stream/namespace")
 				validateDataStreamType(t, doc)
 			},
 		},
@@ -212,8 +214,8 @@ func Test_attributes_data_stream(t *testing.T) {
 			},
 			assertNode: func(t *testing.T, doc *jsonquery.Node) {
 				// no attributes property expected
-				assertJsonValueEqual(t, "warnings", doc, "/attributes/data_stream/namespace")
-				assertJsonValueEqual(t, "mysql", doc, "/attributes/data_stream/dataset")
+				assertJSONValueEqual(t, "warnings", doc, "/attributes/data_stream/namespace")
+				assertJSONValueEqual(t, "mysql", doc, "/attributes/data_stream/dataset")
 
 				validateDataStreamType(t, doc)
 			},
@@ -276,7 +278,7 @@ func Test_sso_routing(t *testing.T) {
 			rec := newBulkRecorder()
 			server := newRecTestServer(t, rec)
 			exporter := newSSOTestTracesExporter(t, logger, server.URL, testCase.configMod)
-			err := exporter.pushTraceRecord(context.TODO(), *sampleResource(), *sampleScope(), sampleSchemaUrl, *sampleSpan())
+			err := exporter.pushTraceRecord(context.TODO(), *sampleResource(), *sampleScope(), sampleSchemaURL, *sampleSpan())
 			require.NoError(t, err)
 			rec.WaitItems(1)
 
@@ -320,3 +322,134 @@ func newRecTestServer(t *testing.T, rec *bulkRecorder) *httptest.Server {
 	})
 	return server
 }
+
+func assertJSONValueEqual(t *testing.T, expected interface{}, node *jsonquery.Node, jq string) {
+	luckyNode := jsonquery.FindOne(node, jq)
+	assert.NotNil(t, luckyNode)
+	assert.Equal(t, expected, luckyNode.Value())
+}
+
+// singleSpanTestCase represents a test case that configures sso exporter with configMod,
+// pushes givenResource and givenSpan and verifies the bulk request that sso exporter generates
+// using assertNode
+type singleSpanTestCase struct {
+	name          string
+	configMod     func(*Config)
+	givenResource *pcommon.Resource
+	givenSpan     *ptrace.Span
+	assertNode    func(*testing.T, *jsonquery.Node)
+	givenScope    *pcommon.InstrumentationScope
+}
+
+func executeTestCase(t *testing.T, testCase *singleSpanTestCase) {
+	t.Run(testCase.name, func(t *testing.T) {
+		logger := zaptest.NewLogger(t)
+
+		ctx := context.TODO()
+		rec := newBulkRecorder()
+		server := newRecTestServer(t, rec)
+		exporter := newSSOTestTracesExporter(t, logger, server.URL, testCase.configMod)
+		err := exporter.pushTraceRecord(ctx, *testCase.givenResource, *testCase.givenScope, sampleSchemaURL, *testCase.givenSpan)
+
+		// Verify that push completed successfully
+		require.NoError(t, err)
+
+		// Wait for HTTP server to capture the bulk request
+		rec.WaitItems(1)
+
+		// Confirm there was only one bulk request
+		require.Equal(t, 1, rec.NumItems())
+		require.Len(t, rec.Requests()[0], 1)
+
+		// Read the bulk request document
+		doc, err := jsonquery.Parse(bytes.NewReader(rec.Requests()[0][0].Document))
+
+		require.NoError(t, err)
+
+		// Verify document matches expectations
+		testCase.assertNode(t, doc)
+	})
+}
+
+func sampleResource() *pcommon.Resource {
+	r := pcommon.NewResource()
+	// An attribute of each supported type
+	r.Attributes().PutStr("service.name", "opensearchexporter")
+	r.Attributes().PutBool("browser.mobile", false)
+	r.Attributes().PutInt("process.pid", 300)
+	return &r
+}
+
+func sampleScope() *pcommon.InstrumentationScope {
+	sample := pcommon.NewInstrumentationScope()
+	sample.Attributes().PutStr("scope.short_name", "com.example.some-sdk")
+	sample.SetDroppedAttributesCount(7)
+	sample.SetName("some-sdk by Example.com")
+	sample.SetVersion("semver:2.5.0-SNAPSHOT")
+	return &sample
+}
+
+func sampleSpan() *ptrace.Span {
+	sp := ptrace.NewSpan()
+
+	sp.SetDroppedAttributesCount(3)
+	sp.SetDroppedEventsCount(5)
+	sp.SetDroppedLinksCount(8)
+	sp.SetSpanID(sampleSpanIDA)
+	sp.SetParentSpanID(sampleSpanIDB)
+	sp.SetTraceID(sampleTraceIDA)
+	sp.SetKind(ptrace.SpanKindInternal)
+	sp.TraceState().FromRaw(rawSampleTraceState)
+	sp.Attributes().PutStr("net.transport", "ip_tcp")
+	sp.SetStartTimestamp(pcommon.NewTimestampFromTime(sampleTimeA))
+	sp.SetEndTimestamp(pcommon.NewTimestampFromTime(sampleTimeB))
+	sp.SetName("sample-span")
+	sp.Status().SetCode(ptrace.StatusCodeError)
+	sp.Status().SetMessage("sample status message")
+	return &sp
+}
+
+func withEventNoTimestamp(span *ptrace.Span) *ptrace.Span {
+	event := span.Events().AppendEmpty()
+	// This event lacks timestamp
+	event.Attributes().PutStr("exception.message", "Invalid user input")
+	event.Attributes().PutStr("exception.type", "ApplicationException")
+	event.SetDroppedAttributesCount(42)
+	event.SetName("exception")
+	return span
+}
+
+func withEventWithTimestamp(span *ptrace.Span) *ptrace.Span {
+	event := span.Events().AppendEmpty()
+	event.Attributes().PutStr("cloudevents.event_id", "001")
+	event.Attributes().PutStr("cloudevents.event_source", "example.com/example-service")
+	event.Attributes().PutStr("cloudevents.event_type", "com.example.example-service.panic")
+	event.SetName("cloudevent")
+	event.SetDroppedAttributesCount(42)
+	event.SetTimestamp(
+		pcommon.NewTimestampFromTime(sampleTimeC))
+	return span
+}
+
+func withSampleLink(span *ptrace.Span) *ptrace.Span {
+	link := span.Links().AppendEmpty()
+	link.SetDroppedAttributesCount(3)
+	link.SetTraceID(sampleTraceIDB)
+	link.SetSpanID(sampleSpanIDB)
+	link.TraceState().FromRaw(rawSampleTraceState)
+	link.Attributes().PutStr("link_attr", "remote-link")
+	return span
+}
+
+const sampleSchemaURL string = "https://example.com/schema/1.0"
+
+var sampleTraceIDA = pcommon.TraceID{1, 0, 0, 0}
+var sampleTraceIDB = pcommon.TraceID{1, 1, 0, 0}
+var sampleSpanIDA = pcommon.SpanID{1, 0, 0, 0}
+var sampleSpanIDB = pcommon.SpanID{1, 1, 0, 0}
+var sampleTimeA = time.Date(1990, 1, 1, 1, 1, 1, 1, time.UTC)
+var sampleTimeB = time.Date(1995, 1, 1, 1, 1, 1, 1, time.UTC)
+var sampleTimeC = time.Date(1998, 1, 1, 1, 1, 1, 1, time.UTC)
+
+// TraceState is defined by a W3C spec. Sample value take from https://www.w3.org/TR/trace-context/#examples-of-tracestate-http-headers
+const rawSampleTraceState = "rojo=00f067aa0ba902b7,congo=t61rcWkgMzE"
